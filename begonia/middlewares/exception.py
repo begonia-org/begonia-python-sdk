e7ba1877ed71bd0531aef688ccb5369c40b28436
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2024-07-15 15:20:30 +0800

    feat:add exception interceptor

--- begonia/middlewares/exception.py
+++ begonia/middlewares/exception.py
@@ -29,31 +29,64 @@ class ExceptionInterceptor(grpc.ServerInterceptor):
         self.resolver: Callable = resolver
         self.log = BegoniaLogger(logger)
 
+    def exception(self, func):
+        def wrapper(req, ctx: grpc.ServicerContext):
+            try:
+                return func(req, ctx)
+            except Exception as e:
+                metadata = dict(ctx.invocation_metadata())
+                metadata["method"] = ctx._rpc_event.call_details.method
+                self.log.error(metadata, str(e))
+                # 在这里可以进行日志记录等操作
+                # print(f"Exception caught in interceptor: {e}")
+                tb = traceback.extract_tb(e.__traceback__)
+                filename, line, func, text = tb[-1]  # 获取最后一条堆栈信息
+                stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
+                # 转换异常为 gRPC 的状态码和详情
+                # context: grpc.ServicerContext = handler_call_details.invocation_metadata
+                code = grpc.StatusCode.INTERNAL
+                msg = "Internal server error"
+                if self.resolver:
+                    code, msg, client_msg = self.resolver(e)
+                details: web_pb2.Errors = web_pb2.Errors(
+                    code=code,
+                    message=msg,
+                    to_client_message=client_msg,
+                    stack=stack,
+                    file=filename,
+                    line=line,
+                    func=func)
+                ctx.abort(code, details)
+        return wrapper
+
     def intercept_service(self, continuation, handler_call_details: grpc.HandlerCallDetails):
-        try:
-            return continuation(handler_call_details)
-        except Exception as e:
-            metadata = dict(handler_call_details.invocation_metadata)
-            metadata["method"] = handler_call_details.method
-            self.log.error(metadata, str(e))
-            # 在这里可以进行日志记录等操作
-            # print(f"Exception caught in interceptor: {e}")
-            tb = traceback.extract_tb(e.__traceback__)
-            filename, line, func, text = tb[-1]  # 获取最后一条堆栈信息
-            stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-            # 转换异常为 gRPC 的状态码和详情
-            # context: grpc.ServicerContext = handler_call_details.invocation_metadata
-            code = grpc.StatusCode.INTERNAL
-            msg = "Internal server error"
-            if self.resolver:
-                code, msg, client_msg = self.resolver(e)
-            details: web_pb2.Errors = web_pb2.Errors(
-                code=code,
-                message=msg,
-                to_client_message=client_msg,
-                stack=stack,
-                file=filename,
-                line=line,
-                func=func)
-            return _unary_unary_rpc_terminator(code, details)
-            # context.abort(code, details)
+        rpc: grpc.RpcMethodHandler = continuation(handler_call_details)
+        rpc.unary_unary = self.exception(rpc.unary_unary)
+        return rpc
+        # try:
+        #     return continuation(handler_call_details)
+        # except Exception as e:
+        #     metadata = dict(handler_call_details.invocation_metadata)
+        #     metadata["method"] = handler_call_details.method
+        #     self.log.error(metadata, str(e))
+        #     # 在这里可以进行日志记录等操作
+        #     # print(f"Exception caught in interceptor: {e}")
+        #     tb = traceback.extract_tb(e.__traceback__)
+        #     filename, line, func, text = tb[-1]  # 获取最后一条堆栈信息
+        #     stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
+        #     # 转换异常为 gRPC 的状态码和详情
+        #     # context: grpc.ServicerContext = handler_call_details.invocation_metadata
+        #     code = grpc.StatusCode.INTERNAL
+        #     msg = "Internal server error"
+        #     if self.resolver:
+        #         code, msg, client_msg = self.resolver(e)
+        #     details: web_pb2.Errors = web_pb2.Errors(
+        #         code=code,
+        #         message=msg,
+        #         to_client_message=client_msg,
+        #         stack=stack,
+        #         file=filename,
+        #         line=line,
+        #         func=func)
+        #     return _unary_unary_rpc_terminator(code, details)
+        # context.abort(code, details)
